a15d850336717c7ac5164a84ad185fd259c2f6b5
JonnyRiver <jonnyriver6@gmail.com>
2022-04-06 17:58:32 -0400

    i think i need to push this

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,18 @@
-const { User, Thought } = require('../models');
+const { User, Thought } = require("../models");
 
 const resolvers = {
   Query: {
     users: async () => {
       return User.find()
-        .select('-__v -password')
-        .populate('thoughts')
-        .populate('friends');
+        .select("-__v -password")
+        .populate("thoughts")
+        .populate("friends");
     },
     user: async (parent, { username }) => {
       return User.findOne({ username })
-        .select('-__v -password')
-        .populate('friends')
-        .populate('thoughts');
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
     },
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
@@ -20,8 +20,16 @@ const resolvers = {
     },
     thought: async (parent, { _id }) => {
       return Thought.findOne({ _id });
-    }
-  }
+    },
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+
+        return user;
+      },
+      login: async () => {},
+    },
+  },
 };
 
 module.exports = resolvers;
